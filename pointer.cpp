a824802fadff8c02eb067796a82175f94ed1bcac
iqbalgn72 <161453083+iqbalgn72@users.noreply.github.com>
2024-05-07 14:50:06 +0700

    Add files via upload

---
+++ pointer.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+class persegiPanjang
+{
+
+public:
+    int panjang, lebar;
+    int luasPersegiPanjang(int b, int a)
+    {
+        return a * b;
+    }
+
+};
+

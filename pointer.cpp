6bdfb32b89cbad0054fde753d3da03c1def50d4d
iqbalgn72 <iqbalgn72@gmail.com>
2024-05-07 15:10:08 +0700

    membuat int main

--- pointer.cpp
+++ pointer.cpp
@@ -3,13 +3,29 @@ using namespace std;
 
 class persegiPanjang
 {
-
 public:
     int panjang, lebar;
     int luasPersegiPanjang(int b, int a)
     {
         return a * b;
     }
-
 };
 
+class segitiga
+{
+public:
+    int alas, tinggi;
+    int luasSegitiga(int a, int t)
+    {
+        return (a * t) / 2;
+    }
+
+    int hitung(persegiPanjang p)
+    {
+        return p.panjang;
+    }
+};
+
+int main(){
+
+}
